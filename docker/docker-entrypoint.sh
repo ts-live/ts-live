ace9e14f5d82fbe1115808142eaf37386ae831db
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2022-02-01 08:25:13 +0900

    entrypoint起動時は毎回acme.sh issueを叩いて期限切れチェックする

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -7,6 +7,7 @@ set -e
   exit 1
 
 export MIRAKURUN_PORT="${MIRAKURUN_PORT:=40772}"
+export NGINX_HTTPS_PORT="${NGINX_HTTPS_PORT:=443}"
 
 [ -z "${ACCOUNT_EMAIL}" -o -z "${DUCKDNS_TOKEN}" ] && \
   echo "Please set ACCOUNT_EMAIL[${ACCOUNT_EMAIL}] and DUCKDNS_TOKEN[${DUCKDNS_TOKEN}]" && \
@@ -22,7 +23,7 @@ export DuckDNS_Token=${DUCKDNS_TOKEN}
 
 [ -f /etc/nginx/nginx.conf ] || cp /nginx-template/conf/nginx.conf /etc/nginx/nginx.conf
 [ -f /etc/nginx/conf.d/ssl.conf ] || \
-  envsubst '${FQDN} ${MIRAKURUN_HOST} ${MIRAKURUN_PORT}' < /nginx-template/conf/conf.d/ssl.conf > /etc/nginx/conf.d/ssl.conf
+  envsubst '${FQDN} ${MIRAKURUN_HOST} ${MIRAKURUN_PORT} ${NGINX_HTTPS_PORT}' < /nginx-template/conf/conf.d/ssl.conf > /etc/nginx/conf.d/ssl.conf
 
 [ -f /etc/nginx/ssl/ssl.conf ] || cp /nginx-template/ssl/ssl.conf /etc/nginx/ssl/ssl.conf
 
@@ -31,10 +32,10 @@ export DuckDNS_Token=${DUCKDNS_TOKEN}
 
 [ -d "/etc/nginx/ssl/${FQDN}" ] || mkdir -p "/etc/nginx/ssl/${FQDN}"
 
-[ -f "/etc/nginx/ssl/${FQDN}/fullchain.cer" ] || (
+[ -f "/etc/nginx/ssl/${FQDN}/fullchain.cer" ] || \
   acme.sh --register-account -m "${ACCOUNT_EMAIL}"
-  acme.sh --issue --dns dns_duckdns -d "${FQDN}"
-)
+
+acme.sh --issue --dns dns_duckdns -d "${FQDN}" || true
 
 busybox crond -b -L /var/log/crond.log
 nginx -g "daemon off;"
