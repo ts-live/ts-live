ffb935196e654d5b5da6d32c289d14df7d9af230
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2022-02-01 22:28:23 +0900

    dnsapiを環境変数で与えるようにする

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -9,11 +9,9 @@ set -e
 export MIRAKURUN_PORT="${MIRAKURUN_PORT:=40772}"
 export NGINX_HTTPS_PORT="${NGINX_HTTPS_PORT:=443}"
 
-[ -z "${ACCOUNT_EMAIL}" -o -z "${DUCKDNS_TOKEN}" ] && \
-  echo "Please set ACCOUNT_EMAIL[${ACCOUNT_EMAIL}] and DUCKDNS_TOKEN[${DUCKDNS_TOKEN}]" && \
+[ -z "${ACCOUNT_EMAIL}" -o -z "${DNSAPI}" ] && \
+  echo "Please set ACCOUNT_EMAIL[${ACCOUNT_EMAIL}] and DNSAPI[${DNSAPI}]" && \
   exit 1
-
-export DuckDNS_Token=${DUCKDNS_TOKEN}
 
 [ -d /etc/nginx/conf.d ] || mkdir -p /etc/nginx/conf.d
 [ -d /etc/nginx/ssl/acme.sh ] || mkdir -p /etc/nginx/ssl/acme.sh
@@ -27,15 +25,12 @@ export DuckDNS_Token=${DUCKDNS_TOKEN}
 
 [ -f /etc/nginx/ssl/ssl.conf ] || cp /nginx-template/ssl/ssl.conf /etc/nginx/ssl/ssl.conf
 
-[ -f /etc/nginx/ssl/acme.sh.conf ] || \
-  envsubst '${ACCOUNT_EMAIL} ${DUCKDNS_TOKEN}' < /nginx-template/ssl/acme.sh.conf > /etc/nginx/ssl/acme.sh.conf
-
 [ -d "/etc/nginx/ssl/${FQDN}" ] || mkdir -p "/etc/nginx/ssl/${FQDN}"
 
 [ -f "/etc/nginx/ssl/${FQDN}/fullchain.cer" ] || \
   acme.sh --register-account -m "${ACCOUNT_EMAIL}"
 
-acme.sh --issue --dns dns_duckdns -d "${FQDN}" || true
+acme.sh --issue --dns "${DNSAPI}" -d "${FQDN}" || true
 
 busybox crond -b -L /var/log/crond.log
 nginx -g "daemon off;"
