b5732b27e1646310b88b536b7715d1b0d6435cbb
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2022-03-04 07:43:51 +0900

    /file

--- src/pages/file/index.tsx
+++ src/pages/file/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import { GitHub } from '@mui/icons-material'
 import Script from 'next/script'
 
@@ -30,23 +30,7 @@ const Home: NextPage = () => {
         </Script>
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          <a href='https://ts-live.pages.dev/'>TS-Live!</a>
-        </h1>
-
-        <div className={styles.grid}>
-          <a href='/live' className={styles.card}>
-            <h2>Live &rarr;</h2>
-            <p>ライブ視聴</p>
-          </a>
-
-          <a href='/file' className={styles.card}>
-            <h2>File &rarr;</h2>
-            <p>ファイル再生</p>
-          </a>
-        </div>
-      </main>
+      <main className={styles.main}>Comming soon...</main>
 
       <footer className={styles.footer}></footer>
     </div>
